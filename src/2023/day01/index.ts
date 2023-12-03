03180c450f3a7f928049dbb77e035968b5cabe0d
Courtney Sims <1546357+csims@users.noreply.github.com>
2023-12-03 00:52:51 -0500

    feat: day 1

---
+++ src/2023/day01/index.ts
@@ -0,0 +1,55 @@
+// https://adventofcode.com/2023/day/1
+
+/**
+ * Part 1:
+ * Input has lines of text.
+ * Each line originally contained a value that needs to be recovered.
+ * The value on each line can be found by combining the first and last digits to form a single two-digit number.
+ * What is the sum of all the values?
+ *
+ * Part 2:
+ * Same as above, but parse number words 'one' thru 'nine'.
+ */
+
+const isNumber = (x: string) => !!Number.parseInt(x, 10)
+
+const numberMap: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
+
+export const day1 = (input: string, transformNumbers = false): number => {
+  const rawLines = input.split('\n').filter(l => !!l)
+
+  const regex = new RegExp(`^(${Object.keys(numberMap).join('|')}|[\\d])`)
+
+  const values: number[] = rawLines.map(line => {
+    let lineNumbers: number[] = []
+
+    for (let i = 0; i < line.length; i++) {
+      const matches = line.slice(i).match(regex)
+      if (matches) {
+        const match = matches[0]
+
+        if (isNumber(match)) {
+          lineNumbers.push(Number.parseInt(match, 10))
+        } else if (transformNumbers) {
+          lineNumbers.push(numberMap[match])
+        }
+      }
+    }
+
+    const first = lineNumbers[0]
+    const last = lineNumbers[lineNumbers.length - 1]
+    return Number.parseInt(`${first}${last}`, 10)
+  })
+
+  return values.reduce((prev, curr) => prev + curr, 0)
+}
