baa9744ff2b523dadbfba313da86ea573c57019d
Courtney Sims <1546357+csims@users.noreply.github.com>
2023-12-07 00:34:52 -0500

    feat: day 4 part 2

--- src/2023/day04/day04.test.ts
+++ src/2023/day04/day04.test.ts
@@ -8,3 +8,11 @@ test('part 1', () => {
   const input = readFileSync(__dirname + '/input.txt', 'utf-8')
   expect(part1(input)).toEqual(26443)
 })
+
+test('part 2', () => {
+  const exampleInput = readFileSync(__dirname + '/inputExample.txt', 'utf-8')
+  expect(part2(exampleInput)).toEqual(30)
+
+  const input = readFileSync(__dirname + '/input.txt', 'utf-8')
+  expect(part2(input)).toEqual(6284877)
+})
--- src/2023/day04/index.ts
+++ src/2023/day04/index.ts
@@ -38,16 +38,37 @@ export const part1 = (input: string): number => {
   const lines = parseLines(input)
   const cards = lines.map(parseCard)
   const points: number[] = cards.map(card => {
-    const winningNumbers = intersection(card.winningNumbers, card.myNumbers)
-    return winningNumbers.size ? Math.pow(2, winningNumbers.size - 1) : 0
+    const matchingNumbers = intersection(card.winningNumbers, card.myNumbers)
+    return matchingNumbers.size ? Math.pow(2, matchingNumbers.size - 1) : 0
   })
 
   return sum(points)
 }
 
 /**
- *
+ * Part 2:
+ * Instead of points, a card's matches gives you copies of the following cards,
+ * e.g. if card 3 has 2 matches, get an extra copy of cards 4 and 5, or if card
+ * 2 has 4 matches, get an extra copy of cards 3, 4, 5, and 6.
+ * Return the total number of scratch cards you end up with.
  */
 export const part2 = (input: string): number => {
-  return 0
+  const lines = parseLines(input)
+  const cards = lines.map(parseCard)
+  const matchingNumberCounts = cards.map(
+    card => intersection(card.winningNumbers, card.myNumbers).size
+  )
+  let cardCopies = new Array(cards.length).fill(1)
+
+  matchingNumberCounts.forEach((matchingCounts, cardIdx) => {
+    const offsetsToCopy = [...Array(matchingCounts + 1).keys()].slice(1)
+
+    offsetsToCopy.forEach(offset => {
+      const oldVal = cardCopies[cardIdx + offset]
+      const newVal = oldVal + cardCopies[cardIdx]
+      cardCopies[cardIdx + offset] = newVal
+    })
+  })
+
+  return sum(cardCopies)
 }
