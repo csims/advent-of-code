49107c8586c5ed72b79441069816efeb9f154989
Courtney Sims <1546357+csims@users.noreply.github.com>
2023-12-06 20:59:04 -0500

    ci: run tsc on push

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const parseLines = (input: string) => input.split('\n').filter(l => !!l)
 
-export const isDefined = <T>(x: T | null | undefined): boolean =>
+export const isDefined = <T>(x: T | null | undefined): x is NonNullable<T> =>
   x !== null && x !== undefined
 
 export const sum = (numbers: number[]) =>
