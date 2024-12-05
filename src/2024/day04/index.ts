ea631bf684a25ecd04de05aa41d0cedebff16dd8
Courtney Sims <1546357+csims@users.noreply.github.com>
2024-12-04 23:23:58 -0500

    do i look like a compass

--- src/2024/day04/index.ts
+++ src/2024/day04/index.ts
@@ -17,8 +17,8 @@ const findPart1Count = (x: number, y: number, grid: string[][]) => {
     [-1, 0], // W
     [1, 0], // E
     [-1, -1], // NW
-    [-1, 1], // NE
-    [1, -1], // SW
+    [-1, 1], // SW
+    [1, -1], // NE
     [1, 1] // SE
   ]
 
